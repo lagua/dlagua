4b375249da0ac216b3e22ea6cb420f6ee70de5fe
wshager <wshager@gmail.com>
2012-11-05 23:05:12 +0100

    updates to 1.8

--- c/store/JsonRest.js
+++ c/store/JsonRest.js
@@ -1,14 +1,13 @@
-define("dlagua/c/store/JsonRest", ["dojo", "dojo.io.script", "dojo.store.JsonRest"], function(dojo) {
-	dojo.declare("dlagua.c.store.JsonRest",[dojo.store.JsonRest],{
+define(["dojo/_base/declare","dojo/_base/lang","dojo/_base/xhr","dojo/_base/Deferred", "dojo/store/JsonRest"], function(declare,lang,xhr,Deferred,JsonRest) {
+	return declare("dlagua/c/store/JsonRest", [JsonRest],{
 		schemaUri:"",
 		hrProperty:"",
 		idProperty:"id",
 		getSchema:function(schemaUri,options){
-			options = options || {};
-			var d = new dojo.Deferred();
+			var d = new Deferred();
 			if(!schemaUri) schemaUri = this.schemaUri;
 			if(schemaUri){
-				var xhr = {
+				xhr("GET",{
 					url:schemaUri,
 					handleAs:"json",
 					headers:{
@@ -25,116 +24,12 @@ define("dlagua/c/store/JsonRest", ["dojo", "dojo.io.script", "dojo.store.JsonRes
 					error:function(){
 						d.callback(false);
 					}
-	    		};
-				if(options.useXDomain) {
-					xhr.callbackParamName = "callback";
-					dojo.io.script.get(xhr);
-				} else {
-					dojo.xhrGet(xhr);
-				}
+	    		});
 			} else {
 				d.callback(false);
 			}
 			return d;
-		},
-		get: function(id, options){
-			//	summary:
-			//		Retrieves an object by its identity. This will trigger a GET request to the server using
-			//		the url `this.target + id`.
-			//	id: Number
-			//		The identity to use to lookup the object
-			//	returns: Object
-			//		The object in the store that matches the given id.
-			var headers = options || {};
-			headers.Accept = "application/json";
-			return dojo.xhrGet({
-				url:this.target + id,
-				handleAs: "json",
-				failOk:true,
-				headers: headers
-			});
-		},
-		put: function(object, options){
-			// summary:
-			//		Stores an object. This will trigger a PUT request to the server
-			//		if the object has an id, otherwise it will trigger a POST request.
-			// object: Object
-			//		The object to store.
-			// options: dojo.store.api.Store.PutDirectives?
-			//		Additional metadata for storing the data.  Includes an "id"
-			//		property if a specific id is to be used.
-			//	returns: Number
-			options = options || {};
-			var id = ("id" in options) ? options.id : this.getIdentity(object);
-			var hasId = typeof id != "undefined";
-			return dojo.xhr(hasId && !options.incremental ? "PUT" : "POST", {
-				url: hasId ? this.target + id : this.target,
-				postData: dojo.toJson(object),
-				handleAs: "json",
-				headers:{
-					"Content-Type": "application/json",
-					"Accept" : "application/json",
-					"If-Match": options.overwrite === true ? "*" : null,
-					"If-None-Match": options.overwrite === false ? "*" : null
-				}
-			});
-		},
-		query: function(query, options){
-			// summary:
-			//		Queries the store for objects. This will trigger a GET request to the server, with the
-			//		query added as a query string.
-			// query: Object
-			//		The query to use for retrieving objects from the store.
-			// options: dojo.store.api.Store.QueryOptions?
-			//		The optional arguments to apply to the resultset.
-			//	returns: dojo.store.api.Store.QueryResults
-			//		The results of the query, extended with iterative methods.
-			var headers = {Accept: "application/json"};
-			options = options || {};
-			
-			if(options.start >= 0 || options.count >= 0){
-				headers.Range = "items=" + (options.start || '0') + '-' +
-					(("count" in options && options.count != Infinity) ?
-						(options.count + (options.start || 0) - 1) : '');
-			}
-			if(dojo.isObject(query)){
-				query = dojo.objectToQuery(query);
-				query = query ? "?" + query: "";
-			}
-			if(options && options.sort){
-				query += (query ? "&" : "?") + "sort(";
-				for(var i = 0; i<options.sort.length; i++){
-					var sort = options.sort[i];
-					query += (i > 0 ? "," : "") + (sort.descending ? '-' : '+') + encodeURIComponent(sort.attribute);
-				}
-				query += ")";
-			}
-			
-			var results;
-			if(options.useXDomain){
-				if(options.start >= 0 || options.count >= 0){
-					query += (query ? "&" : "?") + "limit("+options.count+","+options.start+")";
-				}
-				results = dojo.io.script.get({
-					url:this.target + (query || ""),
-					callbackParamName:"callback",
-					headers:headers
-				});
-			} else {
-				results = dojo.xhrGet({
-					url: this.target + (query || ""),
-					handleAs: "json",
-					headers: headers
-				});
-			}
-			results.total = results.then(function(){
-				if(options.useXDomain) return;
-				var range = results.ioArgs.xhr.getResponseHeader("Content-Range");
-				return range && (range=range.match(/\/(.*)/)) && +range[1];
-			});
-			return dojo.store.util.QueryResults(results);
 		}
 	});
 
-	return dlagua.c.store.JsonRest;
 });
--- c/store/XMLRest.js
+++ c/store/XMLRest.js
@@ -1,13 +1,13 @@
-define("dlagua/c/store/XMLRest", ["dojo", "dojo/store/util/QueryResults"], function(dojo) {
+define(["dojo/_base/declare", "dojo/_base/lang", "dojo/request", "dojo/store/util/QueryResults", "dojo/io-query"], function(declare,lang,request,QueryResults, ioQuery) {
 
-	dojo.declare("dlagua.c.store.XMLRest", null, {
-		constructor: function(/*dojo.store.JsonRest*/ options){
+	return declare("dlagua/store/XMLRest", null, {
+		constructor: function(options){
 			// summary:
 			//		This is a basic store for RESTful communicating with a server through JSON
 			//		formatted data.
 			// options:
 			//		This provides any configuration information that will be mixed into the store
-			dojo.mixin(this, options);
+			declare.safeMixin(this, options);
 		},
 		_query:"",
 		// target: String
@@ -38,9 +38,8 @@ define("dlagua/c/store/XMLRest", ["dojo", "dojo/store/util/QueryResults"], funct
 			var headers = options || {};
 			headers.Accept = "text/xml";
 			var content = {};
-			if(this._query) content = dojo.queryToObject(this._query);
-			return dojo.xhrGet({
-				url:this.target + id,
+			if(this._query) content = ioQuery.queryToObject(this._query);
+			return request(this.target + id,{
 				content:content,
 				handleAs: "xml",
 				failOk:true
@@ -52,14 +51,13 @@ define("dlagua/c/store/XMLRest", ["dojo", "dojo/store/util/QueryResults"], funct
 			//		if the object has an id, otherwise it will trigger a POST request.
 			// object: Object
 			//		The object to store.
-			// options: dojo.store.api.Store.PutDirectives?
+			// options: dojo/store/api/Store/PutDirectives?
 			//		Additional metadata for storing the data.  Includes an "id"
 			//		property if a specific id is to be used.
 			//	returns: Number
 			options = options || {};
-			return dojo.xhr("PUT", {
-				url: this.target + id,
-				postData: data,
+			return request.put(this.target + id,{
+				data: data,
 				handleAs: "xml",
 				headers:{
 					"Content-Type": "text/xml",
@@ -75,14 +73,13 @@ define("dlagua/c/store/XMLRest", ["dojo", "dojo/store/util/QueryResults"], funct
 			//		if the object has an id, otherwise it will trigger a POST request.
 			// object: Object
 			//		The object to store.
-			// options: dojo.store.api.Store.PutDirectives?
+			// options: dojo/store/api/Store/PutDirectives?
 			//		Additional metadata for storing the data.  Includes an "id"
 			//		property if a specific id is to be used.
 			//	returns: Number
 			options = options || {};
-			return dojo.xhr("POST", {
-				url: this.target + oldId,
-				postData: "move:"+newId+".xml",
+			return request.post(this.target + oldId, {
+				data: "move:"+newId+".xml",
 				handleAs: "text"
 			});
 		},
@@ -92,7 +89,7 @@ define("dlagua/c/store/XMLRest", ["dojo", "dojo/store/util/QueryResults"], funct
 			//		if the object has an id, otherwise it will trigger a POST request.
 			// object: Object
 			//		The object to store.
-			// options: dojo.store.api.Store.PutDirectives?
+			// options: dojo/store/api/Store/PutDirectives?
 			//		Additional metadata for storing the data.  Includes an "id"
 			//		property if a specific id is to be used.
 			options = options || {};
@@ -104,9 +101,7 @@ define("dlagua/c/store/XMLRest", ["dojo", "dojo/store/util/QueryResults"], funct
 			//		Deletes an object by its identity. This will trigger a DELETE request to the server.
 			// id: Number
 			//		The identity to use to delete the object
-			return dojo.xhrDelete({
-				url:this.target + id
-			});
+			return request(this.target + id,{method:"delete"});
 		},
 		query: function(query, options){
 			// summary:
@@ -114,9 +109,9 @@ define("dlagua/c/store/XMLRest", ["dojo", "dojo/store/util/QueryResults"], funct
 			//		query added as a query string.
 			// query: Object
 			//		The query to use for retrieving objects from the store.
-			// options: dojo.store.api.Store.QueryOptions?
+			// options: dojo/store/api/Store/QueryOptions?
 			//		The optional arguments to apply to the resultset.
-			//	returns: dojo.store.api.Store.QueryResults
+			//	returns: dojo/store/api/Store/QueryResults
 			//		The results of the query, extended with iterative methods.
 			var headers = {Accept: "text/xml"};
 			options = options || {};
@@ -126,8 +121,8 @@ define("dlagua/c/store/XMLRest", ["dojo", "dojo/store/util/QueryResults"], funct
 					(("count" in options && options.count != Infinity) ?
 						(options.count + (options.start || 0) - 1) : '');
 			}
-			if(dojo.isObject(query)){
-				query = dojo.objectToQuery(query);
+			if(lang.isObject(query)){
+				query = ioQuery.objectToQuery(query);
 				query = query ? "?" + query: "";
 			}
 			if(options && options.sort){
@@ -138,8 +133,7 @@ define("dlagua/c/store/XMLRest", ["dojo", "dojo/store/util/QueryResults"], funct
 				}
 				query += ")";
 			}
-			var results = dojo.xhrGet({
-				url: this.target + (query || ""),
+			var results = request(this.target + (query || ""),{
 				handleAs: "xml",
 				headers: headers
 			});
@@ -147,10 +141,8 @@ define("dlagua/c/store/XMLRest", ["dojo", "dojo/store/util/QueryResults"], funct
 				var range = results.ioArgs.xhr.getResponseHeader("Content-Range");
 				return range && (range=range.match(/\/(.*)/)) && +range[1];
 			});
-			return dojo.store.util.QueryResults(results);
+			return QueryResults(results);
 		}
 	});
 
-
-	return dlagua.c.store.XMLRest;
 });
