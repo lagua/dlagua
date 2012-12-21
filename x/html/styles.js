5211784e65845cad8cd22e8713b9d6f8d8e35104
wshager <wshager@gmail.com>
2012-12-21 03:17:20 +0100

    fixed dlagua.x.html.styles, added getStyleSheet

--- x/html/styles.js
+++ x/html/styles.js
@@ -1,8 +1,8 @@
 dojo.provide("dlagua.x.html.styles");
 dojo.require("dojox.html.styles");
 (function(){
-	dlagua.x.html.styles.insertCssRule = function(/*String*/selector, /*String*/declaration, /*String*/styleSheetName){
-		var ss = dojox.html.getStyleSheet(styleSheetName).ownerNode;
+	dlagua.x.html.insertCssRule = function(/*String*/selector, /*String*/declaration, /*String*/styleSheetName){
+		var ss = dlagua.x.html.getStyleSheet(styleSheetName).ownerNode;
 		if(!ss) ss = dojox.html.getDynamicStyleSheet(styleSheetName);
 		if(!ss._indicies) ss._indicies = [];
 		var styleText = selector + " {" + declaration + "}";
@@ -18,5 +18,21 @@ dojo.require("dojox.html.styles");
 		}
 		ss._indicies.push(selector+" "+declaration);
 		return selector; // String
+	}
+	dlagua.x.html.getStyleSheet = function(/*String*/styleSheetName){
+		if(!styleSheetName) return false;
+		var allSheets = dojox.html.getStyleSheets();
+		// now try document style sheets by name
+		if(allSheets[styleSheetName]){
+			return dojox.html.getStyleSheets()[styleSheetName];
+		}
+		// check for partial matches in hrefs (so that a fully
+		//qualified name does not have to be passed)
+		for(var nm in allSheets){
+			if(allSheets[nm].href && allSheets[nm].href.indexOf(styleSheetName)>-1){
+				return allSheets[nm];
+			}
+		}
+		return false; //StyleSheet or false
 	}
 })();
