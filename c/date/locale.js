f0d638c0825b1979d5a032da9d22b808acb41eca
wshager <wshager@gmail.com>
2013-01-27 01:51:34 +0100

    updated c/date/locale

--- c/date/locale.js
+++ c/date/locale.js
@@ -1,13 +1,15 @@
 define([
-"dojo",
 "dojo/_base/lang",
 "dojo/_base/array",
+"dojo/dom",
 "dojo/i18n",
+"dojo/date",
 "dojo/date/locale",
 "dojo/date/stamp",
+"dojo/aspect",
 "dojox/uuid/generateRandomUuid",
 "dojox/timing"
-],function(dojo,lang,array,i18n,locale,stamp,generateRandomUuid,timing){
+],function(lang,array,dom,i18n,date,locale,stamp,aspect,generateRandomUuid,timing){
 
 lang.getObject("dlagua.c.date.locale", true);
 
@@ -20,34 +22,37 @@ var format = function(val,options) {
 				date = new Date(parseInt(val,10)*60*1000);
 			break;
 			default:
-				date = new dojo.date.stamp.fromISOString(val);
+				date = new stamp.fromISOString(val);
 			break;
 		}
 	} else {
-		date = new dojo.date.stamp.fromISOString(val);
+		date = new stamp.fromISOString(val);
 	}
 	if(options._ref.ref && options._ref.ref.locale) options.locale = options._ref.ref.locale.replace("_","-");
 	if(!options.ignoreZeroTimes || date.getHours()+date.getMinutes()+date.getSeconds()>0){
-		oritxt = txt = dojo.date.locale.format(date,options);
+		oritxt = txt = locale.format(date,options);
 	}
 	if(options.autoupdate) {
-		var id = dojox.uuid.generateRandomUuid();
+		var id = generateRandomUuid();
 		var interval = (options.updateInterval || 30*1000);
 		var limit = (options.updateLimit || 7*24*60*60*1000);
 		var now = new Date();
-		var diff = dojo.date.difference(date,now,"millisecond");
+		var diff = date.difference(date,now,"millisecond");
 		var thresholds = (options.updateThresholds || [60*1000,60*60*1000,24*60*60*1000,7*24*60*60*1000,30*24*60*60*1000]);
 		// TODO: from locale
-		var locale = dojo.i18n.normalizeLocale(options.locale);
-		var lb = dojo.i18n.getLocalization("lagua.date", "interval", locale);
+		var locale = i18n.normalizeLocale(options.locale);
+		var lb = i18n.getLocalization("dlagua.c.date", "interval", locale);
 		var markers = (options.updateMarkers || ["{moments} {ago}","{value} {minute} {ago}","{value} {hour} {ago}","{value} {day} {ago}","{value} {week} {ago}"]);
 		if(diff<limit) {
-			var i=0;
-			for(;i<thresholds.length;i++) {
-				if(diff<thresholds[i]) break;
+			var x = 0;
+			for(var i=0;i<thresholds.length;i++) {
+				if(diff<thresholds[i]) {
+					x = i;
+					break;
+				}
 			}
-			var val = Math.round(diff/thresholds[i-1]);
-			var repl = dojo.mixin(lb,{
+			var val = Math.round(diff/thresholds[x-1]);
+			var repl = lang.mixin(lb,{
 				value:val,
 				second:(val==1 ? lb.second : lb.seconds),
 				minute:(val==1 ? lb.minute : lb.minutes),
@@ -55,19 +60,22 @@ var format = function(val,options) {
 				day:(val==1 ? lb.day : lb.days),
 				week:(val==1 ? lb.week : lb.weeks)
 			});
-			txt = '<span id="'+id+'">'+dojo.replace(markers[i],repl)+'</span>';
-			var timer = new dojox.timing.Timer(interval);
+			txt = '<span id="'+id+'">'+lang.replace(markers[x],repl)+'</span>';
+			var timer = new timing.Timer(interval);
 			timer.start();
-			var tc = dojo.connect(timer,"onTick",this,function(){
+			var tc = aspect.after(timer,"onTick",this,function(){
 				var now = new Date();
-				var diff = dojo.date.difference(date,now,"millisecond");
-				var i=0;
+				var diff = date.difference(date,now,"millisecond");
 				if(diff<limit) {
-					for(;i<thresholds.length;i++) {
-						if(diff<thresholds[i]) break;
+					var x = 0;
+					for(var i=0;i<thresholds.length;i++) {
+						if(diff<thresholds[i]) {
+							x = i;
+							break;
+						}
 					}
-					var val = Math.round(diff/thresholds[i-1]);
-					var repl = dojo.mixin(lb,{
+					var val = Math.round(diff/thresholds[x-1]);
+					var repl = lang.mixin(lb,{
 						value:val,
 						second:(val==1 ? lb.second : lb.seconds),
 						minute:(val==1 ? lb.minute : lb.minutes),
@@ -75,17 +83,17 @@ var format = function(val,options) {
 						day:(val==1 ? lb.day : lb.days),
 						week:(val==1 ? lb.week : lb.weeks)
 					});
-					txt = dojo.replace(markers[i],repl);
+					txt = lang.replace(markers[x],repl);
 				} else {
 					txt = oritxt;
-					dojo.disconnect(tc);
+					tc.remove();
 				}
-				var node = dojo.byId(id);
+				var node = dom.byId(id);
 				if(node) {
 					node.innerHTML = txt;
 				} else {
 					// we lost the node, clear it
-					dojo.disconnect(tc);
+					tc.remove();
 				}
 			});
 		}
