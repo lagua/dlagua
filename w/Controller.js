34b366440215f1a834cb7642d7394a010e5bfe0e
wshager <wshager@gmail.com>
2013-04-26 11:43:10 +0200

    controller added

---
+++ w/Controller.js
@@ -0,0 +1,12 @@
+define([
+	"dojo/_base/declare", // declare
+	"dojo/_base/lang", // lang.getObject
+	"dijit/_WidgetBase",
+	"dijit/_Container",
+	"dijit/_Contained"
+], function(declare, lang, _WidgetBase, _Container, _Contained){
+
+	return declare("dlagua.w.Controller", [_WidgetBase,_Container,_Contained], {
+	});
+	
+});
