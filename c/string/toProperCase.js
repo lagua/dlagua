6ce27f5ea9c4456e30011882794813ab4d8f1711
wshager <wshager@gmail.com>
2013-05-24 14:28:00 +0200

    added force lowercase option

--- c/string/toProperCase.js
+++ c/string/toProperCase.js
@@ -1,11 +1,13 @@
 define("dlagua/c/string/toProperCase", ["dojo/_base/lang"], function(lang) {
 lang.getObject("dlagua.c.string", true);
-dlagua.c.string.toProperCase = function(/*String*/ value){
-	return value.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
+dlagua.c.string.toProperCase = function(/*String*/ value, force){
+	return value.replace(/\w\S*/g, function(txt){
+		return txt.charAt(0).toUpperCase() + (force ? txt.substr(1).toLowerCase() : txt.substr(1));
+	});
 };
 
-String.prototype.toProperCase = function() {
-	return dlagua.c.string.toProperCase(this);
+String.prototype.toProperCase = function(force) {
+	return dlagua.c.string.toProperCase(this,force);
 }
 
 return dlagua.c.string.toProperCase;
