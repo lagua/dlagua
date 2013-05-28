8f5d2554ca07b850b5bf8b5cc097a93c52edeb1a
wshager <wshager@gmail.com>
2013-05-28 14:32:24 +0200

    inherit from StatefulController

--- w/DropDownMenu.js
+++ w/DropDownMenu.js
@@ -1,17 +1,18 @@
 define([
 	"dojo/_base/declare",
+	"dojo/_base/array",
 	"dijit/DropDownMenu",
 	"dlagua/w/TreeController",
-	"dlagua/w/Resolvable"
-],function(declare,DropDownMenu,TreeController,Resolvable) {
-	return declare("dlagua.w.DropDownMenu",[DropDownMenu,TreeController,Resolvable],{
-		childType:"dlagua/w/PopupMenuItem",
+	"dlagua/w/StatefulController",
+	"dlagua/w/MenuItem"
+],function(declare,array,DropDownMenu,TreeController,StatefulController,MenuItem) {
+	return declare("dlagua.w.DropDownMenu",[DropDownMenu,TreeController,StatefulController],{
 		maxDepth:2,
 		_itemNodesMap:null,
 		labelAttr:"title",
-		startup:function(){
-			var data = this.resolve(this.item,self.store);
-			array.forEach(data.children,this._addItem,this);
+		childWidget:MenuItem,
+		onItemClick:function(item) {
+			this.selectNode(item);
 			this.inherited(arguments);
 		}
 	});
