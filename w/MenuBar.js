613d93220f33abdebfa5e1af21cb1285b3edca0f
wshager <wshager@gmail.com>
2013-06-05 11:40:38 +0200

    added menu selectNode handle to reset selectedNode of dropdown

--- w/MenuBar.js
+++ w/MenuBar.js
@@ -1,33 +1,38 @@
 define([
 	"dojo/_base/declare",
 	"dojo/_base/lang",
+	"dojo/aspect",
 	"dijit/MenuBar",
 	"dlagua/w/DropDownMenu",
 	"dlagua/w/TreeController",
 	"dlagua/w/StatefulController",
 	"dlagua/w/MenuBarItem",
 	"dlagua/w/PopupMenuBarItem"
-],function(declare,lang,MenuBar,DropDownMenu,TreeController,StatefulController,MenuBarItem,PopupMenuBarItem){
+],function(declare,lang,aspect,MenuBar,DropDownMenu,TreeController,StatefulController,MenuBarItem,PopupMenuBarItem){
 	return declare("dlagua.w.MenuBar",[MenuBar,TreeController,StatefulController],{
 		childWidget:MenuBarItem,
 		store:null,
 		depth:2,
-		onItemHover: function(item){
-			this.inherited(arguments);
-		},
 		_addItem:function(item,index,items,params){
-			var self = this;
 			if(!item._loadObject && this.maxDepth>2 && item.children && item.children.length && !params) {
+				var dd = new DropDownMenu({
+					store:this.store,
+					currentItem:item,
+					publishId:this.id,
+					depth:3,
+					parent:this,
+					maxDepth:this.maxDepth,
+					labelAttr:this.labelAttr
+				});
+				this.own(aspect.after(this,"selectNode",function(node){
+					if(dd!=node.popup && dd._selectedNode) {
+						dd._selectedNode.set("selected",false);
+						dd._selectedNode = null;
+					}
+				},true));
 				this._addItem(item,index,items,{
 					childWidget:PopupMenuBarItem,
-					popup:new DropDownMenu({
-						store:self.store,
-						currentItem:item,
-						publishId:self.id,
-						depth:3,
-						maxDepth:self.maxDepth,
-						labelAttr:self.labelAttr
-					})
+					popup:dd
 				});
 			} else {
 				this.inherited(arguments);
