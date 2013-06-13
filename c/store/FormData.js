a72c1688e9abafd92ed25ec3791f5706f726d4d7
W.S. Hager <wshager@gmail.com>
2013-06-13 23:41:52 +0200

    added formdata

---
+++ c/store/FormData.js
@@ -0,0 +1,41 @@
+define([
+	"dojo/_base/declare",
+	"dojo/_base/lang",
+	"dojo/store/Observable",
+	"./JsonRest", 
+	"./LocalStore"], function(declare,lang,Observable,JsonRest,LocalStore) {
+	
+	return declare("dlagua.c.store.FormData",null,{
+		idProperty: "id",
+		model:"",
+		schemaModel:"Class",
+		service:"/persvr/",
+		local:false,
+		persistent:false,
+		constructor: function(options) {
+			this.headers = {};
+			declare.safeMixin(this, options);
+			var model = this.model;
+			var schemaModel = this.schemaModel;
+			var target = this.service+model+"/";
+			var schemaUri = this.service+schemaModel+"/"+model;
+			var store;
+			if(this.local) {
+				store = new Observable(new LocalStore({
+					idProperty: this.idProperty,
+					persistent:this.persistent,
+					target:target,
+					schemaUri:schemaUri
+				}));
+			} else {
+				store = new Observable(new JsonRest({
+					idProperty: this.idProperty,
+					target:target,
+					schemaUri:schemaUri
+				}));
+			}
+			declare.safeMixin(this,store);
+		}
+	});
+	
+});
