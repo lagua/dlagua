f061673da1444a386cceada70b2361736027d49d
wshager <wshager@gmail.com>
2013-06-24 15:40:57 +0200

    added store for new tree object model

---
+++ c/store/Resolvable.js
@@ -0,0 +1,10 @@
+define(["dojo/_base/declare","dojo/_base/lang","dojo/Deferred","dojo/store/JsonRest", "dojo/store/Observable"], function(declare,lang,Deferred,JsonRest,Observable) {
+	return declare("dlagua.c.store.Resolvable",[JsonRest,Observable],{
+		childrenAttr:"children",
+		getChildren:function(item){
+			var d = new Deferred();
+			d.resolve(item[this.childrenAttr]);
+			return d.
+		}
+	});
+});
