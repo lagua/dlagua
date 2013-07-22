f95d33b64200d9717e35f5309f5aadb05190a8bd
whager <whager@benjamins.nl>
2013-07-22 13:03:23 +0200

    added children resolver

--- c/store/Resolvable.js
+++ c/store/Resolvable.js
@@ -1,9 +1,25 @@
-define(["dojo/_base/declare","dojo/_base/lang","dojo/Deferred","dojo/store/JsonRest", "dojo/store/Observable"], function(declare,lang,Deferred,JsonRest,Observable) {
+define([
+	"dojo/_base/declare",
+	"dojo/_base/lang",
+	"dojo/_base/array",
+	"dojo/Deferred",
+	"dojo/store/JsonRest",
+	"dojo/store/Observable"
+], function(declare,lang,array,Deferred,JsonRest,Observable) {
 	return declare("dlagua.c.store.Resolvable",[JsonRest,Observable],{
 		childrenAttr:"children",
 		getChildren:function(item){
 			var d = new Deferred();
-			d.resolve(item[this.childrenAttr]);
+			var children = array.map(item[this.childrenAttr],function(_){
+				_.__parent = item;
+				if(_[this.childrenAttr]) {
+					this.getChildren(_).then(function(c){
+						_.children = c;
+					});
+				}
+				return _;
+			},this);
+			d.resolve(children);
 			return d;
 		}
 	});
