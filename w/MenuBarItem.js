c599243b2581fc9d37c81e1baa568f575d863428
whager <whager@benjamins.nl>
2013-08-05 12:22:06 +0200

    hide node using dijitHidden class

--- w/MenuBarItem.js
+++ w/MenuBarItem.js
@@ -1,12 +1,13 @@
 define([
 	"dojo/_base/declare",
+	"dojo/dom-class",
 	"dijit/MenuBarItem"
-],function(declare,MenuBarItem) {
+],function(declare,domClass,MenuBarItem) {
 	return declare("dlagua.w.MenuBarItem",[MenuBarItem],{
 		selected:false,
 		item:null,
 		postCreate:function(){
-			if(this.item && this.item.hidden) this.domNode.style.display = "none";
+			if(this.item && this.item.hidden) domClass.add(this.domNode,"dijitHidden");
 			this.inherited(arguments);
 		},
 		_setSelected: function(selected){
