3ca3765ba754d0d4cd9947685c401925c4e56b7c
W.S. Hager <wshager@gmail.com>
2014-02-18 17:56:49 +0100

    added reference to this for parser, fixed reference to parent widget

--- w/Parsable.js
+++ w/Parsable.js
@@ -3,14 +3,16 @@ define([
 	"dojo/_base/declare",
 	"dojo/_base/lang",
 	"dojo/dom",
+	"dijit/registry",
 	"dijit/_WidgetBase",
 	"dijit/_TemplatedMixin"
-],function(require,declare,lang,dom,_WidgetBase,_TemplatedMixin){
+],function(require,declare,lang,dom,registry,_WidgetBase,_TemplatedMixin){
 return declare("dlagua.w.Parsable",[_WidgetBase,_TemplatedMixin],{
 	postscript:function(params,refNode){
 		if(!params) return;
 		var self = this;
 		var args = arguments;
+		params._parsable = this;
 		if(params.type){
 			var node, type, method;
 			node = dom.byId(refNode);
@@ -21,6 +23,11 @@ return declare("dlagua.w.Parsable",[_WidgetBase,_TemplatedMixin],{
 				delete params.method;
 			} else if(type.indexOf("::")>-1) {
 				var ar = type.split("::");
+				if(params.ref) {
+					if(typeof params.ref == "string") params.ref = registry.byId(params.ref);
+				} else {
+					params.ref = registry.getEnclosingWidget(refNode);
+				}
 				type = ar[0];
 				method = ar[1];
 			} else {
