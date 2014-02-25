849950dd6a125f93581659514be2998ec0120681
whager <whager@benjamins.nl>
2014-02-25 16:15:55 +0100

    moved xform to form

--- w/layout/FormPane.js
+++ w/layout/FormPane.js
@@ -2,10 +2,10 @@ define([
 	"dojo/_base/declare",
 	"dojo/_base/lang",
 	"dlagua/w/layout/_ScrollableServicedPane",
-	"dlagua/w/layout/_XFormMixin",
+	"dlagua/w/layout/_FormMixin",
 	"dlagua/w/Subscribable"
 ], function(declare,lang, _SSP, _FormMixin, Subscribable) {
 	
-	return declare("dlagua.w.layout.FormPane",[_SSP,_XFormMixin, Subscribable],{});
+	return declare("dlagua.w.layout.FormPane",[_SSP,_FormMixin, Subscribable],{});
 
 });
