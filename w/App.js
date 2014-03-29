608f5e59ac233700571f2c8ef22de5a9af5c5570
whager <whager@benjamins.nl>
2014-03-29 22:08:52 +0100

    added view-change notification

--- w/App.js
+++ w/App.js
@@ -2,20 +2,33 @@ define([
 	"dojo/_base/declare",
 	"dojo/_base/lang",
 	"dojo/_base/array",
-	"dojo/on",
+	"dojo/topic",
 	"dijit/layout/LayoutContainer",
+	"dijit/Viewport",
 	"dlagua/c/App",
 	"dlagua/w/Subscribable"
-],function(declare,lang,array,on,LayoutContainer,App,Subscribable){
+],function(declare,lang,array,topic,LayoutContainer,Viewport,App,Subscribable){
 
 return declare("dlagua.w.App", [LayoutContainer,App,Subscribable], {
+	flipSize:890,
+	_checkFlip:function(){
+		var box = Viewport.getEffectiveBox();
+		var f = box.w<=this.flipSize;
+		if(f!=this.flipped) {
+			console.log("emit")
+			topic.publish("/components/"+this.id+"/view-change", f ? "small" : "large");
+		}
+		this.flipped = f;
+	},
 	startup: function(){
 		this.own(
-			on(window,"onresize",lang.hitch(this,function(){
+			Viewport.on("resize",lang.hitch(this,function(){
+				this._checkFlip();
 				this.resize();
 			}))
 		);
 		this.inherited(arguments);
+		this._checkFlip();
 		this.resize();
 	}
 });
