1805f83c238a5eabb70c43f23bf2c8b20e27738f
whager <whager@benjamins.nl>
2014-05-09 14:56:17 +0200

    use nativeScroll by default (removed Scrollable)

--- w/layout/ScrollableServicedPane.js
+++ w/layout/ScrollableServicedPane.js
@@ -7,16 +7,16 @@
  */
 define([
 	"dojo/_base/declare",
-	"dlagua/x/mobile/Scrollable",
+	//"dlagua/x/mobile/Scrollable",
 	"dlagua/w/layout/_ScrollableServicedPane",
 	"dlagua/w/layout/_FormMixin",
 	"dlagua/w/layout/_ModelMixin",
 	"dlagua/w/layout/_PageKeysMixin",
 	"dlagua/w/layout/_PagedMixin",
 	"dlagua/w/Subscribable"
-],function(declare,Scrollable,_SSP,_FormMixin,_ModelMixin,_PageKeysMixin,_PagedMixin,Subscribable){
-return declare("dlagua.w.layout.ScrollableServicedPane",[_SSP, Scrollable, _FormMixin, _ModelMixin, _PageKeysMixin, _PagedMixin, Subscribable],{
-	nativeScroll:false,
+],function(declare,_SSP,_FormMixin,_ModelMixin,_PageKeysMixin,_PagedMixin,Subscribable){
+return declare("dlagua.w.layout.ScrollableServicedPane",[_SSP, _FormMixin, _ModelMixin, _PageKeysMixin, _PagedMixin, Subscribable],{
+	nativeScroll:true,
 	noCover:false
 });
 
