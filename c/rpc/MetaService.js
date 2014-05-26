6e0d8a03f1ac3d08df06136487f04d191334159d
whager <whager@benjamins.nl>
2014-05-26 17:35:51 +0200

    updated to use /model/Node/

--- c/rpc/MetaService.js
+++ c/rpc/MetaService.js
@@ -1,21 +1,25 @@
 define([
 	"dojo/_base/lang",
+	"dojo/io-query",
 	"dojo/request"
-], function(lang,request) {
+], function(lang,ioQuery,request) {
 
 lang.getObject("dlagua.c.rpc", true);
 
 var MetaService = function(data) {
 	var result = {};
-	request(data.target,{
+	var query = data.query;
+	var hasQ = query && query.charAt(0)=="?";
+	var qstr = query ? hasQ ? query : "?" + query : "";
+	request(data.target+qstr,{
 		sync:true,
 		handleAs:"json",
-		header:{
+		headers:{
 			accept:"application/json",
 			"content-type":"application/json"
 		}
 	}).then(function(res){
-		result[data.property] = res[0].start.data;
+		result[data.property] = res[0].data;
 		data.ref.meta = dojo.mixin(data.ref.meta,result);
 		// replace all meta to replace any loaded variables
 		for(var k in data.ref.nodes){
