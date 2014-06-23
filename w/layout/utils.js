de165efd8735739e595e7f2fca5f18163a7893c5
W.S. Hager <wshager@gmail.com>
2014-06-23 22:51:47 +0200

    first attempt at child progs

--- w/layout/utils.js
+++ w/layout/utils.js
@@ -56,18 +56,25 @@ define([
 			};
 		},
 		
-		calcRegion:function(children,dim,max,changedRegionId,changedRegionSize){
+		calcRegion:function(region,dim,changedRegionId,changedRegionSize){
 			// set positions/sizes
+			// - if auto and fits ignore everything else (including min)
+			// - else if not fits, update curProg until 5
+			// if 5 and fits (it will) reset curProg to 1 so
+			// lower prios will be fitted again
 			var progs = {
 				0:"none",
-				1:"default",
+				1:"auto",
 				2:"fixed",
-				3:"minmax",
+				3:"min",
 				4:"tile",
 				5:"hide"
 			}
+			var children = region.children;
+			var max = region.max;
 			var len = children.length;
 			var fit = true;
+			var tileSize = this.tileSize;
 			array.forEach(children, function(child,i){
 				var elm = child.domNode,
 					pos = child.region;
@@ -90,8 +97,8 @@ define([
 				}
 				
 				var prog = child._prog || 0;
-				
-				var nextprog = i<len-1 ? children[i+1]._prog : 0;
+				console.log(child.id)
+				//var nextprog = i<len-1 ? children[i+1]._prog : 0;
 				// set size && adjust record of remaining space.
 				// note that setting the width of a <div> may affect its height.
 				if(pos == "top" || pos == "bottom"){
@@ -104,30 +111,68 @@ define([
 						elmStyle.top = dim.t + dim.h + "px";
 					}
 				}else if(pos == "left" || pos == "right"){
-					if(prog<nextprog) {
-						// what to do?
-						prog++;
-					}
-					console.log("prog",prog)
 					// check if elm has height
 					// check if elmHeight <= dim.h, if not refit
 					// if fits, size to elm height and try to fit below
-					if(child._dim.height) {
-						this._prog = 2;
-						if(child._dim.height<dim.h) {
-							// fit, my prog will be 2
-							sizeSetting.h = child._dim.height;
-						} else {
-							// nofit!
-							// - if minHeight: prog will be 3
-							// - else if tile prog will be 4
-							// - else prog will be 5
-							fit = false;
-							return;
+					// prog: either it wasn't fitted or it was fitted, but must refit (prog<2<curProg)
+					// if curProg >=5 stop
+					if(!prog || (prog && prog===region.prog-2)) {
+						if(region.prog<2) {
+							if(child._dim.height) {
+								// fixed!
+								if(child._dim.height<=dim.h) {
+									console.log("fixed!")
+									child._prog = 2;
+									sizeSetting.h = child._dim.height;
+								} else {
+									// nofit!
+									// - if minHeight: prog will be 3
+									// - else if tile prog will be 4
+									// - else prog will be 5
+									child._prog = 0;
+									region.prog++;
+									fit = false;
+									return;
+								}
+							} else {
+								// else auto-height
+								console.log("auto!")
+								child._prog = 1;
+								sizeSetting.h = dim.h;
+							}
+						} else if(region.prog<3) {
+							// min
+							if(child._dim.minHeight && child._dim.minHeight<=dim.h) {
+								console.log("min!")
+								child._prog = 3;
+								sizeSetting.h = dim.h;
+							} else {
+								// nofit!
+								child._prog = 0;
+								region.prog++;
+								fit = false;
+								return;
+							}
+						} else if(region.prog<4) {
+							// tilesize
+							if(tileSize && tileSize<=dim.h) {
+								console.log("tile!")
+								child._prog = 4;
+								sizeSetting.h = tileSize;
+							} else {
+								// nofit!
+								child._prog = 0;
+								region.prog++;
+								fit = false;
+								return;
+							}
+						} else if(region.prog<5) {
+							// hide
+							console.log("hide!")
+							child._prog = 5;
+							sizeSetting.h = 0;
+							region.prog = 1;
 						}
-					} else {
-						// else auto-height
-						sizeSetting.h = dim.h;
 					}
 					size(child, sizeSetting);
 					dim.w -= child.w;
@@ -148,6 +193,7 @@ define([
 					size(child, dim);
 				}
 			});
+			console.log(fit,region.prog)
 			return fit ? dim : false;
 		},
 
@@ -203,7 +249,8 @@ define([
 					pos = child.isLeftToRight() ? "right" : "left";
 				}
 				child.region = pos;
-				if(!regions[pos]) regions[pos] = [];
+				child._prog = 0;
+				if(!regions[pos]) regions[pos] = {id:pos,children:[],prog:1};
 				return child;
 			});
 			
@@ -220,17 +267,14 @@ define([
 			// - min-size
 			// - tileSize
 			// - if still not fits, hide smart
-			console.log(dim)
 			for(var region in regions){
-				regions[region] = array.filter(children,function(_){
+				regions[region].children = array.filter(children,function(_){
 					return _.region == region;
 				});
-			//}
-			//for(var region in regions){
 				// get max width/height
 				var prop = (region == "top" || region == "bottom") ? "height" : "width";
 				var val = 0;
-				array.forEach(regions[region], function(child,i){
+				array.forEach(regions[region].children, function(child,i){
 					// retrieve all dimension styles
 					// store the first time
 					var elm = child.domNode;
@@ -239,14 +283,12 @@ define([
 					// - setStyle: extend dom-style to emit set+elm (extend _WidgetBase to include domNode getters/setters)
 					if(!child._dim) {
 						var style = elm.style;
-						console.log(style)
 						var id = elm.id;
 						var classes = elm.className.split(" ");
 						var rules = rulestore.query("#"+id);
 						classes.forEach(function(_){
 							rules = rules.concat(rulestore.query("."+_));
 						});
-						console.log(rules);
 						child._dim = {
 							"width":"",
 							"height":"",
@@ -262,11 +304,12 @@ define([
 					}
 					val = Math.max(val,Math.max(child._dim[prop],child._dim["min"+capitalize(prop)]));
 				});
+				regions[region].max = val;
 				// don't update dim when nofit
 				var fit = null;
 				var safe = 6*children.length;
 				while(!fit && safe>0) {
-					fit = utils.calcRegion(regions[region],dim,val,changedRegionId, changedRegionSize);
+					fit = utils.calcRegion(regions[region],dim,changedRegionId, changedRegionSize);
 					safe--;
 				}
 				dim = fit;
