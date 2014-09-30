6e5ca16ee892af9173db8a55f406c052eab70b82
wshager <wshager@gmail.com>
2014-09-30 17:29:45 +0200

    set model root at load

--- w/TreeMenu.js
+++ w/TreeMenu.js
@@ -69,14 +69,14 @@ define([
 					if(!reload) return;
 				}
 			}
-			console.log("TreeMenu rebuilding currentItem ",this.currentItem)
-			this.model.root = lang.mixin({},this.currentItem);
 			this.rebuild();
 		},
 		_load: function(){
 			// summary:
 			//		Initial load of the tree.
 			//		Load root node (possibly hidden) and it's children.
+			// always load currentItem as root, it will never come from a query
+			this.model.root = lang.mixin({},this.currentItem);
 			this.model.getRoot(
 				lang.hitch(this, function(item){
 					if(!item) {
