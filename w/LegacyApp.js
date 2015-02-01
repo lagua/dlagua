793577f73eeeddd7e5b33b9337526c126cdb9ebd
W.S. Hager <wshager@gmail.com>
2015-02-01 15:23:27 +0100

    added legacyapp based on LayoutContainer

---
+++ w/LegacyApp.js
@@ -0,0 +1,12 @@
+define([
+	"dojo/_base/declare",
+	"dojo/_base/lang",
+	"dijit/layout/LayoutContainer",
+	"dlagua/c/App",
+	"dlagua/w/Subscribable"
+],function(declare,lang,LayoutContainer,App,Subscribable){
+
+return declare("dlagua.w.App", [LayoutContainer,App,Subscribable], {
+});
+
+});
