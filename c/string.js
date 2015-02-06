439d45d81e6b696f4aaecd37ece97017e7270996
W.S. Hager <wshager@gmail.com>
2015-02-06 10:27:39 +0100

    added array replacement

--- c/string.js
+++ c/string.js
@@ -11,7 +11,26 @@ var exports = {
 lang.setObject(module.id.replace(/\//g, "."), exports);
 
 exports.replace = function(val,options) {
-	return val.replace(options.pattern,options.replace);
+	var pat = options.pattern;
+	var rep = options.replace;
+	if(typeof pat=="string" && typeof rep=="string"){
+		val = val.replace(options.pattern,rep);
+	} else if(pat instanceof Array && rep instanceof Array){
+		for(var i=0,l=pat.length;i<l;i++){
+			val = val.replace(pat[i],rep[i] || "");
+		}
+	} else if(typeof rep=="object" && rep instanceof Array){
+		val = val.replace(options.pattern,function(){
+			var args = Array.prototype.slice.call(arguments);
+			var match = args.shift();
+			var ret = "";
+			for(var i=0,l=args.length-2;i<l;i++){
+				ret += (rep[i] || ""); 
+			}
+			return ret;
+		});
+	}
+	return val;
 };
 
 return exports;
