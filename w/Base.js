630d7cf7c7649c255e183d374e37265052d8bdb9
wshager <wshager@gmail.com>
2015-03-31 12:44:31 +0200

    added base widget

---
+++ w/Base.js
@@ -0,0 +1,39 @@
+define([
+	"dojo/_base/array",
+	"dojo/_base/declare",
+	"dijit/_Contained",
+	"dijit/_WidgetBase",
+	"./Subscribable"
+], function(array, declare, Contained, WidgetBase,Subscribable){
+
+	// module:
+	//		dojox/mobile/Pane
+
+	return declare("dojox.mobile.Pane", [WidgetBase, Contained, Subscribable], {
+		// summary:
+		//		A simple pane widget.
+		// description:
+		//		Pane is a simple general-purpose pane widget.
+		//		It is a widget, but can be regarded as a simple `<div>` element.
+
+		// baseClass: String
+		//		The name of the CSS class of this widget.
+		baseClass: "mblPane",
+
+		buildRendering: function(){
+			this.inherited(arguments);
+			if(!this.containerNode){
+				// set containerNode so that getChildren() works
+				this.containerNode = this.domNode;
+			}
+		},
+
+		resize: function(){
+			// summary:
+			//		Calls resize() of each child widget.
+			array.forEach(this.getChildren(), function(child){
+				if(child.resize){ child.resize(); }
+			});
+		}
+	});
+});
